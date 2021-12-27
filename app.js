f9b3d4fc2e0bfb593c6795c6fcf0f86e2f3df3c9
Lazer Porter <lazar.porter@gmail.com>
2021-12-27 14:34:15 -0500

    first basic routes

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressgraphql = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello from node");
-});
+app.use(
+  '/graphql',
+  expressgraphql.graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery { 
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Sunrise Yoga', 'Nature Trail Picnic', 'Cooking'];
+      },
+      createEvent: (args) => {
+        const { name: eventName } = args;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
