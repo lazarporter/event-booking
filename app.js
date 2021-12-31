2785a5d832cf201f1d242dfd981f8ed352850a0d
Lazer Porter <lazar.porter@gmail.com>
2021-12-30 22:38:34 -0500

    add first types, working mutation, in-memory initial 'database'

--- app.js
+++ app.js
@@ -6,15 +6,30 @@ const { buildSchema } = require('graphql');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.use(
   '/graphql',
   expressgraphql.graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         type RootQuery { 
-            events: [String!]!
+            events: [Event!]!
         }
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
         schema {
             query: RootQuery
@@ -23,11 +38,18 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Sunrise Yoga', 'Nature Trail Picnic', 'Cooking'];
+        return events;
       },
       createEvent: (args) => {
-        const { name: eventName } = args;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
